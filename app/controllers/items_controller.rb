86d001da68390144cf1d547a397dc39c6f1e7990
brandonkwong <brandonjk@gmail.com>
2014-08-26 11:04:00 -0700

    Setup Item model and index form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+    @item = Item.new
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :tag_list)
+  end
+
 end
