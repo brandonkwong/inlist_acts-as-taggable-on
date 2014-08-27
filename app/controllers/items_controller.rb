e4714cbed333794c03aecc3dc2106b8b67679f1f
brandonkwong <brandonjk@gmail.com>
2014-08-26 23:35:15 -0700

    Added tags to form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,19 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
     @item = Item.new
+    @items_test = Item.where(tag_list: 'asdf')
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
 
   def item_params
     params.require(:item).permit(:name, :tag_list)
